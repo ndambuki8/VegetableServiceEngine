5c505f467d5056e95a28a3df65a417560c11a67e
ndambuki <ndambukikevo8@gmail.com>
2020-08-16 00:09:33 +0300

    Setting up the servlets classes

---
+++ src/CalculateCost.java
@@ -0,0 +1,19 @@
+public class CalculateCost {
+
+
+    public static void main(String args[])
+    {
+        String message = "still gat it";
+        System.out.println(message);
+    }
+
+
+
+
+
+}
+
+
+
+
+
